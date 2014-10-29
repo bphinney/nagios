b05c0e52633ae21f1da0ff5f095337ef394a28e0
Bryan Phinney <bryan.phinney@prometheanworld.com>
2014-10-29 10:10:55 -0400

    Updated to correctly remove nomonitoring environments from search query generated hostgroups.

--- recipes/default.rb
+++ recipes/default.rb
@@ -197,7 +197,7 @@ if nagios_bags.bag_list.include?('nagios_hostgroups')
     temp_hostgroup_array = Array.new
     if node['nagios']['multi_environment_monitoring']
       search(:node, hg['search_query']) do |n|
-        unless noenvirons.any?{ |str| environment.name.include? str }
+        unless noenvirons.any?{ |str| n.chef_environment.include? str }
           temp_hostgroup_array << n[node['nagios']['host_name_attribute']]
         end
       end
