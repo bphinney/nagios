13e8d4eca76c0e50b46ab77683356291cb895cc0
Bryan Phinney <bryan.phinney@prometheanworld.com>
2014-10-29 10:03:31 -0400

    Updated to try to exclude nomonitored environments from hostgroup queries.

--- recipes/default.rb
+++ recipes/default.rb
@@ -197,7 +197,9 @@ if nagios_bags.bag_list.include?('nagios_hostgroups')
     temp_hostgroup_array = Array.new
     if node['nagios']['multi_environment_monitoring']
       search(:node, hg['search_query']) do |n|
-        temp_hostgroup_array << n[node['nagios']['host_name_attribute']]
+        unless noenvirons.any?{ |str| environment.name.include? str }
+          temp_hostgroup_array << n[node['nagios']['host_name_attribute']]
+        end
       end
     else
       search(:node, "#{hg['search_query']} AND chef_environment:#{node.chef_environment}") do |n|
